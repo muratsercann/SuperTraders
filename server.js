17b507bf54d1e7384f6a5122f8c9964134b8c0c0
Murat Sercan <muratsercann@gmail.com>
2024-05-25 00:39:22 +0300

    implementing api routes for trading into server.js

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = require("./src/routes/userRoutes");
+const userRoutes = require("./src/routes/userRoutes");
+const tradeRoutes = require("./src/routes/tradeRoutes");
 const sequelize = require("./src/database/db");
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
-app.use("/api/users", router);
+// app.use("/api/users", userRoutes);
+app.use("/api/trade", tradeRoutes);
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
