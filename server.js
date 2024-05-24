777a72956510dfab776f33abf0cc68d66bba858d
Murat Sercan <muratsercann@gmail.com>
2024-05-24 17:11:43 +0300

    server.js

--- server.js
+++ server.js
@@ -6,18 +6,8 @@ const sequelize = require("./src/database/db");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// sequelize.sync({ force: true })
-//   .then(() => {
-//     console.log('Model veritabanıyla senkronize edildi.');
-//   })
-//   .catch(err => {
-//     console.error('Modeli senkronize ederken bir hata oluştu:', err);
-//   });
-
 app.use(bodyParser.json());
 app.use("/api/users", router);
-
-
 
 app.listen(PORT, () => {
   console.log(`API is listening on port ${PORT}`);
