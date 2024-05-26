8e6459fb4cfa185b86022bc98e3ff5a5244b3459
Murat Sercan <muratsercann@gmail.com>
2024-05-26 12:41:42 +0300

    Validation for api/buy inputs

--- src/routes/tradeRoutes.js
+++ src/routes/tradeRoutes.js
@@ -3,8 +3,32 @@ const tradeController = require("../controllers/tradeController");
 
 const router = express.Router();
 
-router.post("/buy/", tradeController.createBuyOrder);
+const validateInputs = (req, res, next) => {
+  const { userId, shareId, quantity } = req.body;
 
-router.post("/sell/", tradeController.createSellOrder);
+  if (!Number.isInteger(userId) || userId <= 0) {
+    return res
+      .status(400)
+      .json({ error: "Invalid userId, must be a positive integer" });
+  }
+
+  if (!Number.isInteger(shareId) || shareId <= 0) {
+    return res
+      .status(400)
+      .json({ error: "Invalid shareId, must be a positive integer" });
+  }
+
+  if (!Number.isInteger(quantity) || quantity <= 0) {
+    return res
+      .status(400)
+      .json({ error: "Invalid quantity, must be a positive integer" });
+  }
+
+  next();
+};
+
+router.post("/buy/", validateInputs, tradeController.createBuyOrder);
+
+// router.post("/sell/", validateInput, tradeController.createSellOrder);
 
 module.exports = router;
